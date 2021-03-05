14665b10242fb681f3abb819b2b7e74b4f0ca2ea
RVolpe <kild_ro@hotmail.com>
2021-03-05 16:39:25 -0300

    created class Conta

--- api-collections/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,11 +2,17 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Rodrigo"
+
     var cpf: String = "123.123.123-49"
+    private set    //cpf não pode ser alterado externamente, somente lido (get)
+
+    constructor()
+
+    fun pessoaInfo() = "Nome: $nome - CPF: $cpf"
 }
 
 fun main() {
     val rodrigo = Pessoa()
 
-    println( "Nome: "+ rodrigo.nome + " - CPF: " + rodrigo.cpf)
+    println(rodrigo.pessoaInfo())
 }
